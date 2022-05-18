a41b6bc8009977f5c701cb91929e9bfd09a28dc9
Lakshya Mehrishi <76775443+LuckyBoy03@users.noreply.github.com>
2022-05-18 12:09:16 -0400

    Update script.js

--- script.js
+++ script.js
@@ -59,3 +59,14 @@ document.addEventListener("scroll", () => {
 });
 
 backToTopButton.addEventListener("click", goToTop);
+
+
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbz0LjIYrO7V5b_wOHVMXctBsc-lg2Fe-Bjjaf4DI5v2B6yg7X2TRa35xAmdgC-1wdwCrA/exec'
+const form = document.forms['google-sheet']
+form.addEventListener('submit', e => {
+e.preventDefault()
+fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+.then(response => $("#form_alerts").html("<div class='alert alert-success'>Contact message sent successfully.</div>"))
+.catch(error => $("#form_alerts").html("<div class='alert alert-danger'>Contact message not sent.</div>"))
+})
